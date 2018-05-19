8515a1be3b62ee53957ff44fa65158eceaf3fcf4
31151023348 <indo287@gmail.com>
2018-05-20 02:17:19 +0700

    31151023348

---
+++ JS/Cau1.js
@@ -0,0 +1,40 @@
+﻿function rePlay() {
+    for (var i = 1; i <= 300; i++) {
+        setTimeout(taosongaunhien, 50 * i);
+    }
+}
+function taosongaunhien() {
+    value_a = parseInt(document.getElementsByName("giatri")[0].value);
+    document.getElementsByName("kq")[0].value = parseInt(Math.random() * (value_a) + 1);
+}
+function rePlay1() {
+    for (var i = 1; i <= 300; i++) {
+        setTimeout(taodaysongaunhien, 50 * i);
+    }
+}
+function taodaysongaunhien() {
+    document.getElementsByName("kq")[0].value = '';
+    value_a = parseInt(document.getElementsByName("giatri")[0].value);
+    var b = new Array(value_a);
+    var c = '';
+    var m = '';
+    var n = '';
+    var temp = '';
+    for (var i = 0; i < b.length; i++) {
+        b[i] = i + 1;
+    }
+    for (var j = 0; j < b.length; j++) {
+        m = parseInt(Math.random() * (value_a));
+        n = parseInt(Math.random() * (value_a));
+        temp = b[m];
+        b[m] = b[n];
+        b[n] = temp;
+    }
+    for (var k = 0; k < b.length; k++) {
+		if (k == b.length - 1)
+		{ c += b[k]; }
+		else
+        { c += b[k] + ','; }
+    }
+    document.getElementsByName("kq")[0].value += c;
+}
---
+++ JS/Cau3.js
@@ -0,0 +1,82 @@
+﻿function kiemtra() {
+	var loi = true;
+	hoten = document.getElementById("name");
+	ht = hoten.value;
+	if (ht == "") {
+		document.getElementById("errname").innerHTML = "Họ tên không được để trống ";
+		hoten.style.backgroundColor = "yellow";
+		loi = (loi && false);
+	}
+	else {
+		document.getElementById("errname").innerHTML = "";
+		hoten.style.backgroundColor = "Green"
+	}
+	
+	sodt = document.getElementById("sdt");
+	dt = sodt.value;
+	var khonghople = /^[0-9-+]+$/;
+	if (!khonghople.test(dt)) {
+		document.getElementById("errsdt").innerHTML = "Số điện thoại không được để trống hoặc số điện này có chứa một số ký tự không phù hợp";
+		sodt.style.backgroundColor = "yellow";
+		loi = (loi && false);
+	}
+
+	else {
+		document.getElementById("errsdt").innerHTML = "";
+		sodt.style.backgroundColor = "Green"
+	}
+	
+	tinhtp = document.getElementById("tinh");
+	tt = tinhtp.value;
+	if (tt == "") {
+		document.getElementById("errtinh").innerHTML = "Vui lòng chọn tỉnh thành phố để xác định vị trí của bạn";
+		tinhtp.style.backgroundColor = "yellow";
+		loi = (loi && false);
+
+	}
+	else {
+		document.getElementById("errtinh").innerHTML = "";
+		tinhtp.style.backgroundColor = "Green"
+	}
+	
+	duongdc = document.getElementById("duong");
+	dg = duongdc.value;
+	if (dg == "") {
+		document.getElementById("errduong").innerHTML = "Đường không được để trống không được để trống ";
+		duongdc.style.backgroundColor = "yellow";
+		loi = (loi && false);
+	}
+	else {
+		document.getElementById("errduong").innerHTML = "";
+		duongdc.style.backgroundColor = "Green"
+	}
+	
+	d_chi = document.getElementById("diachi");
+	dc = d_chi.value;
+	if (dc == "") {
+		document.getElementById("errdiachi").innerHTML = "Đại chỉ không được để trống không được để trống ";
+		d_chi.style.backgroundColor = "yellow";
+		loi = (loi && false);
+	}
+	else {
+		document.getElementById("errdiachi").innerHTML = "";
+		d_chi.style.backgroundColor = "Green"
+	}
+	
+	gg = document.getElementById("magg");
+	ma = gg.value
+	var khonghp = /^[0-9-+]+$ /;
+	if (!khonghople.test(ma)) {
+		document.getElementById("errgg").innerHTML = "Mã giảm giá này không hợp lệ";
+		gg.style.backgroundColor = "yellow";
+		loi = (loi && false);
+	} else {
+		document.getElementById("errgg").innerHTML = "";
+		gg.style.backgroundColor = "Green"
+	}
+	if (loi == true) {
+		document.getElementById("myform").submit();
+		alert("Đã đăng kí thành công !");
+	}
+
+}
